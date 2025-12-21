4703913035114490bd24496d981e807234fc77cd
Yasmim Muniz <yasmim.c.muniz@gmail.com>
2025-12-21 15:27:59 -0300

    Implementação dos testes unitarios

--- FCG.Game.Tests/GameServiceTests.cs
+++ FCG.Game.Tests/GameServiceTests.cs
@@ -44,5 +44,97 @@ namespace FCG.Game.Tests
                 g.Id == gameId && g.Title == dto.Title
             )), Times.Once);
         }
+
+        [Fact]
+        public async Task GetGameByIdAsync_ReturnsGame_WhenRepositoryReturnsGame()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var game = new FCG.Game.Domain.Entities.Game(id, "T", "D", "G", 10m, "P", DateTime.UtcNow, new List<string>(), "");
+            _gameRepositoryMock.Setup(r => r.GetGameByIdAsync(id)).ReturnsAsync(game);
+
+            // Act
+            var result = await _gameService.GetGameByIdAsync(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(id, result!.Id);
+        }
+
+        [Fact]
+        public async Task SearchGamesAsync_DelegatesToRepository()
+        {
+            // Arrange
+            var term = "foo";
+            var page = 1;
+            var pageSize = 10;
+            var games = new List<FCG.Game.Domain.Entities.Game>
+            {
+                new FCG.Game.Domain.Entities.Game(Guid.NewGuid(), "A", "D", "G", 1m, "P", DateTime.UtcNow, new List<string>(), "")
+            };
+
+            _gameRepositoryMock.Setup(r => r.SearchGamesAsync(term, page, pageSize)).ReturnsAsync(games);
+
+            // Act
+            var result = await _gameService.SearchGamesAsync(term, page, pageSize);
+
+            // Assert
+            Assert.Same(games, result);
+            _gameRepositoryMock.Verify(r => r.SearchGamesAsync(term, page, pageSize), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetGamesByGenreAsync_DelegatesToRepository()
+        {
+            // Arrange
+            var genre = "Action";
+            var limit = 5;
+            var games = new List<FCG.Game.Domain.Entities.Game>();
+            _gameRepositoryMock.Setup(r => r.GetGamesByGenreAsync(genre, limit)).ReturnsAsync(games);
+
+            // Act
+            var result = await _gameService.GetGamesByGenreAsync(genre, limit);
+
+            // Assert
+            Assert.Same(games, result);
+            _gameRepositoryMock.Verify(r => r.GetGamesByGenreAsync(genre, limit), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetRecommendationsAsync_UsesMostPopular_WhenCalled()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var limit = 3;
+            var games = new List<FCG.Game.Domain.Entities.Game>
+            {
+                new FCG.Game.Domain.Entities.Game(Guid.NewGuid(), "A", "D", "G", 1m, "P", DateTime.UtcNow, new List<string>(), "")
+            };
+
+            _gameRepositoryMock.Setup(r => r.GetMostPopularGamesAsync(limit)).ReturnsAsync(games);
+
+            // Act
+            var result = await _gameService.GetRecommendationsAsync(userId, limit);
+
+            // Assert
+            Assert.Same(games, result);
+            _gameRepositoryMock.Verify(r => r.GetMostPopularGamesAsync(limit), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetMostPopularGamesAsync_DelegatesToRepository()
+        {
+            // Arrange
+            var limit = 7;
+            var games = new List<FCG.Game.Domain.Entities.Game>();
+            _gameRepositoryMock.Setup(r => r.GetMostPopularGamesAsync(limit)).ReturnsAsync(games);
+
+            // Act
+            var result = await _gameService.GetMostPopularGamesAsync(limit);
+
+            // Assert
+            Assert.Same(games, result);
+            _gameRepositoryMock.Verify(r => r.GetMostPopularGamesAsync(limit), Times.Once);
+        }
     }
 }
--- FCG.Game.Tests/GamesControllerTests.cs
+++ FCG.Game.Tests/GamesControllerTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FCG.Game.API.Controllers;
 using FCG.Game.Application.DTOs;
 using FCG.Game.Application.Services.Interfaces; // Add this using
+using System.Linq;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -73,6 +74,111 @@ namespace FCG.Game.Tests
             
             // Verify service was called
             _gameServiceMock.Verify(s => s.CreateGameAsync(dto), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetGame_ReturnsNotFound_WhenServiceReturnsNull()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _gameServiceMock.Setup(s => s.GetGameByIdAsync(id)).ReturnsAsync((FCG.Game.Domain.Entities.Game?)null);
+
+            // Act
+            var result = await _gamesController.GetGame(id);
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task GetGame_ReturnsOk_WithGameDto_WhenServiceReturnsGame()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var game = new FCG.Game.Domain.Entities.Game(id, "Title", "Desc", "Genre", 9.99m, "Pub", DateTime.UtcNow, new List<string>(), "");
+            _gameServiceMock.Setup(s => s.GetGameByIdAsync(id)).ReturnsAsync(game);
+
+            // Act
+            var result = await _gamesController.GetGame(id);
+
+            // Assert
+            var ok = result.Should().BeOfType<OkObjectResult>().Subject;
+            var dto = ok.Value.Should().BeOfType<GameDto>().Subject;
+            dto.Id.Should().Be(id);
+            dto.Title.Should().Be("Title");
+        }
+
+        [Fact]
+        public async Task SearchGames_ReturnsPagedResult_WithExpectedShape()
+        {
+            // Arrange
+            var term = "test";
+            var page = 2;
+            var pageSize = 5;
+            var games = new List<FCG.Game.Domain.Entities.Game>
+            {
+                new FCG.Game.Domain.Entities.Game(Guid.NewGuid(), "A", "D", "G", 1m, "P", DateTime.UtcNow, new List<string>(), ""),
+                new FCG.Game.Domain.Entities.Game(Guid.NewGuid(), "B", "D", "G", 2m, "P", DateTime.UtcNow, new List<string>(), "")
+            };
+
+            _gameServiceMock.Setup(s => s.SearchGamesAsync(term, page, pageSize)).ReturnsAsync(games);
+
+            // Act
+            var result = await _gamesController.SearchGames(term, page, pageSize);
+
+            // Assert
+            var ok = result.Should().BeOfType<OkObjectResult>().Subject;
+            var value = ok.Value!;
+            var pageProp = value.GetType().GetProperty("page");
+            pageProp.Should().NotBeNull();
+            ((int)pageProp.GetValue(value)!).Should().Be(page);
+
+            var pageSizeProp = value.GetType().GetProperty("pageSize");
+            pageSizeProp.Should().NotBeNull();
+            ((int)pageSizeProp.GetValue(value)!).Should().Be(pageSize);
+
+            var totalProp = value.GetType().GetProperty("total");
+            totalProp.Should().NotBeNull();
+            ((int)totalProp.GetValue(value)!).Should().Be(games.Count);
+
+            var dataProp = value.GetType().GetProperty("data");
+            dataProp.Should().NotBeNull();
+            var dataVal = (IEnumerable<object>)dataProp.GetValue(value)!;
+            dataVal.Count().Should().Be(games.Count);
+        }
+
+        [Fact]
+        public async Task GetRecommendations_ReturnsOk_WithRecommendationsAndUserId()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var limit = 3;
+            SetupUserClaims(userId.ToString(), "Usuario");
+
+            var games = new List<FCG.Game.Domain.Entities.Game>
+            {
+                new FCG.Game.Domain.Entities.Game(Guid.NewGuid(), "A", "D", "G", 1m, "P", DateTime.UtcNow, new List<string>(), "")
+            };
+
+            _gameServiceMock.Setup(s => s.GetRecommendationsAsync(userId, limit)).ReturnsAsync(games);
+
+            // Act
+            var result = await _gamesController.GetRecommendations(limit);
+
+            // Assert
+            var ok = result.Should().BeOfType<OkObjectResult>().Subject;
+            var value = ok.Value!;
+            var userIdProp = value.GetType().GetProperty("userId");
+            userIdProp.Should().NotBeNull();
+            ((Guid)userIdProp.GetValue(value)!).Should().Be(userId);
+
+            var countProp = value.GetType().GetProperty("count");
+            countProp.Should().NotBeNull();
+            ((int)countProp.GetValue(value)!).Should().Be(games.Count);
+
+            var recProp = value.GetType().GetProperty("recommendations");
+            recProp.Should().NotBeNull();
+            ((IEnumerable<object>)recProp.GetValue(value)!).Should().NotBeNull();
         }
     }
 }
